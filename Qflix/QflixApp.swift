a7ec5e9ddb230e71093ce47dcbdef2db2815479b
deep2000 <mddeep22@gmail.com>
2023-10-08 18:23:37 +0600

    Initial Commit

---
+++ Qflix/QflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  QflixApp.swift
+//  Qflix
+//
+//  Created by Talut mahamud Deep on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct QflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
